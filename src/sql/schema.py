932966f342d7590d318d6d03b7e9d812fd2be966
jayc <26672467+jaycxz@users.noreply.github.com>
2023-10-29 15:12:47 +0000

    refactor(fetcher): clean up fetcher

--- src/sql/schema.py
+++ src/sql/schema.py
@@ -1,28 +1,33 @@
 from typing import List
 
-from sqlalchemy import DATE, BigInteger, Column, Float, ForeignKey, Integer, String
+from sqlalchemy import DATE, BigInteger, Float, ForeignKey, Integer, String
 from sqlalchemy.orm import Mapped, declarative_base, mapped_column, relationship
 
 Base = declarative_base()
 
+
 class Ticker(Base):
-    __tablename__  = "ticker"
+    __tablename__ = "ticker"
     id = mapped_column(BigInteger(), primary_key=True, autoincrement=True)
     name = mapped_column(String, unique=True, nullable=False)
     datas: Mapped[List["Data"]] = relationship(back_populates="ticker")
 
+
 class Timeframe(Base):
-    __tablename__  = "timeframe"
+    __tablename__ = "timeframe"
     id = mapped_column(BigInteger(), primary_key=True, autoincrement=True)
     name = mapped_column(String, unique=True, nullable=False)
     datas: Mapped[List["Data"]] = relationship(back_populates="timeframe")
+
 
 class Data(Base):
     __tablename__ = "data"
 
     date: Mapped[DATE] = mapped_column(DATE, primary_key=True)
     ticker_id = mapped_column(Integer(), ForeignKey("ticker.id"), primary_key=True)
-    timeframe_id = mapped_column(Integer(), ForeignKey("timeframe.id"), primary_key=True)
+    timeframe_id = mapped_column(
+        Integer(), ForeignKey("timeframe.id"), primary_key=True
+    )
     open: Mapped[Float] = mapped_column(Float)
     high: Mapped[Float] = mapped_column(Float)
     low: Mapped[Float] = mapped_column(Float)
